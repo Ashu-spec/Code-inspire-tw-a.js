f11179436016eec13c64e90cb5fdfd9f8a1b0cc1
Ashish Christian <christianashish050@gmail.com>
2022-10-27 12:40:15 +0530

    new

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss')
+const tailwindcss = require('tailwindcss');
 
 mix.js('src/js/app.js', 'assets')
    .sass('src/scss/app.scss', 'assets')
